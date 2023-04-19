fa32dd48d2aebe584aa41b3abd42abe63e698549
michi <reischlmichael3@gmail.com>
2023-04-19 12:19:18 +0200

    startseite a wenge bessa

--- app/src/main/java/com/example/projekt_unfallbericht/newActivity.java
+++ app/src/main/java/com/example/projekt_unfallbericht/newActivity.java
@@ -134,4 +134,10 @@ public class newActivity extends AppCompatActivity {
 
 
     }
+
+    public void btnZurueck(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
 }
